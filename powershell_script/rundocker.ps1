4cbe3de87201c11d1d96e8ebbc053174c158009a
s1blik <ranno.mannikust@gmail.com>
2025-12-21 14:15:29 +0200

    added postgres

--- powershell_script/rundocker.ps1
+++ powershell_script/rundocker.ps1
@@ -1,5 +1,22 @@
-﻿docker compose down
+﻿#docker compose down
 
 docker compose up -d
+
+#--volume käsud--
+
+#volume nägemine
+docker volume ls
+
+#volume kontroll
+docker volume inspect microservices_pgdata
+
+#volume kustutamine
+<#
+docker volume rm microservices_pgdata
+
+Või kõik kasutamata volume’id:
+docker volume prune
+#>
+
 
 
